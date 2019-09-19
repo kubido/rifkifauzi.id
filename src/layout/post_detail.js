3bbabff8ae487335717be885913ce328b90e67b1
Rifki Fauzi <kubido@gmail.com>
2019-09-19 13:53:35 +0300

    update seo

--- src/layout/post_detail.js
+++ src/layout/post_detail.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Layout from '../layout/main'
 import { FacebookProvider, Comments } from 'react-facebook';
-
+import SEO from "../components/seo"
 
 export default ({data, location}) => {
   const post = data.markdownRemark
 
   return(
+    
     <Layout>
+      <SEO title={post.frontmatter.title}/>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
       <FacebookProvider appId="776869216027510">
         <Comments href={location.href} />
